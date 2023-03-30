d2e439dbdb389e1562079949687b114ee4314d9e
id.exalt@gmail.com <id.exalt@gmail.com>
2023-03-31 06:31:55 +0800

    add dayr rate

--- plugins/dayr-rate.js
+++ plugins/dayr-rate.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-	let url = 'https://telegra.ph/file/c78b2ae0ccc5e2b355623.jpg'
+	let url = 'https://telegra.ph/file/f65347b433bf1cbe604c8.jpg'
 let wm = 'Hai Saya Ex-Bot, rate ini di buat febuary 2023'
 let cap = `*🅓︎ 🅐︎ 🅨︎ 🅡︎ - 🅡︎ 🅐︎ 🅣︎ 🅔︎*`
 	conn.sendButton(m.chat, cap, wm, await(await fetch(url)).buffer(), [['🅓︎🅐︎🅨︎',`🅡︎`]],m)
